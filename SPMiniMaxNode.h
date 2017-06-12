0d541d06898a9b20b3dbdf8e5be998ba52407dfb
Michael Svit <michaelsvit@gmail.com>
2017-06-12 19:14:03 +0300

    Add function declarations to header file and update unit test

--- SPMiniMaxNode.h
+++ SPMiniMaxNode.h
@@ -3,6 +3,10 @@
 #ifndef SPMINIMAXNODE_H_
 #define SPMINIMAXNODE_H_
 
+void scoreScanRows(SPFiarGame* game, int* scoring);
+void scoreScanCols(SPFiarGame* game, int* scoring);
+void scoreScanDownDiag(SPFiarGame* game, int* scoring);
+void scoreScanUpDiag(SPFiarGame* game, int* scoring);
 int score(SPFiarGame* game);
 
 #endif
